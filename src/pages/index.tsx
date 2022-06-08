4fcd5af33ba01c30e7e537c32ac9b18b83cabc0c
Richard Ruiter <richard.ruiter@gmail.com>
2022-06-08 21:54:55 +0200

    stripped starter

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const Home = () => (
+  <Layout>
+    <h1>Candidates</h1>
+    <p>Hi</p>
+  </Layout>
+);
+
+export default Home;
