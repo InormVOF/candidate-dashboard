55f962762e1f0aa85bdd98a8be68a5cb9f331489
Richard Ruiter <richard.ruiter@gmail.com>
2022-08-25 19:16:46 +0200

    trigger onload for chat script

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,19 +7,27 @@ interface Props {
   children: React.ReactNode;
 }
 
-const Layout = ({ header = null, children }: Props) => (
-  <div>
-    <header className="pb-2">
-      <nav className="flex items-center justify-between max-w-screen-xl p-4 mx-auto">
-        <a className="inline-flex items-center justify-center" href="/">
-          <img src={logo} className="h-14" />
-        </a>
-        {header}
-      </nav>
-    </header>
-    <main>{children}</main>
-    <Script src="https://embed.small.chat/T03R6AEGURZC040EJKBW00.js" />
-  </div>
-);
+const Layout = ({ header = null, children }: Props) => {
+  const chatScriptLoaded = () => {
+    dispatchEvent(new Event("load"));
+  };
+  return (
+    <div>
+      <header className="pb-2">
+        <nav className="flex items-center justify-between max-w-screen-xl p-4 mx-auto">
+          <a className="inline-flex items-center justify-center" href="/">
+            <img src={logo} className="h-14" />
+          </a>
+          {header}
+        </nav>
+      </header>
+      <main>{children}</main>
+      <Script
+        src="https://embed.small.chat/T03R6AEGURZC040EJKBW00.js"
+        onLoad={() => chatScriptLoaded()}
+      />
+    </div>
+  );
+};
 
 export default Layout;
